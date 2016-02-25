c69cfb366ad2638cb2b9d2ffc65fa074c4da58ab
= <=>
2016-02-24 17:50:35 -0800

    Initial Commit of City Scene

---
+++ City-Scene/Nack/CarAnimate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAnimate : MonoBehaviour {
+
+
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(30, 0, 0, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
